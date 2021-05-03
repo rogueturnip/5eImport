4948a9fcff4d76d88d32190f4e963af47a002d45
Tony Mauro <tony@plasmatic.ai>
2021-05-03 13:33:03 -0700

    update

---
+++ classes.js
@@ -0,0 +1,80 @@
+import fs from "fs";
+import _ from "lodash";
+import { default as mongodb } from "mongodb";
+
+const uri =
+  "mongodb+srv://testUser:testPassword@cluster0.vbl9h.mongodb.net/?retryWrites=true&w=majority";
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const main = async () => {
+  const directoryPath =
+    "/home/tony/projects/TheGiddyLimit.github.io/data/class";
+  const classFiles = fs.readdirSync(directoryPath);
+  try {
+    await Promise.all(
+      classFiles.map((classFile) => {
+        console.log("class file", classFile);
+        if (classFile.startsWith("class")) {
+          const classItem = JSON.parse(
+            fs.readFileSync(`${directoryPath}/${classFile}`, "utf8")
+          ).class;
+          const classFeature = JSON.parse(
+            fs.readFileSync(`${directoryPath}/${classFile}`, "utf8")
+          ).classFeature;
+          const subclassFeature = JSON.parse(
+            fs.readFileSync(`${directoryPath}/${classFile}`, "utf8")
+          ).subclassFeature;
+          console.log(classItem);
+        }
+      })
+    );
+    // try {
+    //   const items = JSON.parse(fs.readFileSync(itemsFile, "utf8")).item;
+    //   const fluff = JSON.parse(fs.readFileSync(fluffFile, "utf8")).itemFluff;
+    //   console.log(`count items ${items.length} fluff ${fluff.length}`);
+    //   const newItems = await Promise.all(
+    //     items.map((item) => {
+    //       let itemFluff = {};
+    //       if (item.hasFluffImages) {
+    //         itemFluff = _.find(fluff, { name: item.name });
+    //       }
+    //       return {
+    //         id: `${item.name.replace(/\W/g, "")}-${item.source}-${
+    //           item.page
+    //         }`.toLowerCase(),
+    //         base: false,
+    //         // ..._.merge(item, itemFluff),
+    //         ...item,
+    //         images: itemFluff?.images || [],
+    //       };
+    //     })
+    //   );
+    //   return newItems;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+(async () => {
+  try {
+    await client.connect();
+    const dbo = client.db("5e");
+    var text = await main();
+    // await Promise.all(
+    //   text.map(async (item) => {
+    //     const query = { id: item.id };
+    //     const update = { $set: item };
+    //     const options = { upsert: true };
+    //     await dbo.collection("items").updateOne(query, update, options);
+    //   })
+    // );
+  } catch (e) {
+    // Deal with the fact the chain failed
+    console.log(e);
+  } finally {
+    await client.close();
+  }
+})();
